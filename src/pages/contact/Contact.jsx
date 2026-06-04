fc7100d6108979986ca955cdf769453e87801fa5
MdSahilIslam <islamsahil2001@gmail.com>
2026-06-04 19:19:16 +0530

    fixing images

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,13 @@
 import Material from "../home/material";
 import Testimonial from "../home/testimonial";
+import cntcImg from "../../assets/contact-background.jpg";
 
 function Contact() {
   return (
     <section className=" min-h-screen">
       <div
         className="w-full h-[400px] bg-cover bg-center text-center flex items-center justify-center"
-        style={{ backgroundImage: "url(./src/assets/contact-background.jpg)" }}
+        style={{ backgroundImage: `url(${cntcImg})` }}
       >
         <h1 className="text-black/60 text-6xl font-bold">Contact Us</h1>
       </div>
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,12 @@
 import Product from "./product";
+import heroImg from "../../assets/banner.png";
 
 function Shop() {
   return (
     <section className=" min-h-screen">
       <div
         className="w-full h-[400px] bg-cover bg-center text-center flex items-center justify-center"
-        style={{ backgroundImage: "url(./src/assets/banner.png)" }}
+        style={{ backgroundImage: `url(${heroImg})` }}
       >
         <h1 className="text-white text-6xl font-bold">Shop Our Product</h1>
       </div>
